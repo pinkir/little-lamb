5442b93181a8e080b479ec297678d0563b2d6334
pinkir <pinki.roy@outlook.com>
2023-05-18 15:34:23 +0600

    tailwind added

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <div>
+        <button className="btn btn-secondary">Button</button>
+        <button className="btn btn-accent">Button</button>
+        
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
