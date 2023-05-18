2dbab55f3a624cdd8829da57fb12b25ef7d8a17a
pinkir <pinki.roy@outlook.com>
2023-05-18 19:51:01 +0600

    navbar footer added

---
+++ src/Shared/Header.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/littlelamb.jpg'
+
+const Header = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/alltoys'>All Toys</Link></li>
+                    <li><Link to='/mytoys'>My Toys</Link></li>
+                    <li><Link to='/addatoy'>Add A Toy</Link></li>
+                    <li><Link to='/blogs'>Blogs</Link></li>
+                    </ul>
+                </div>
+                <Link to='/' className="btn btn-ghost normal-case text-xl" > <img className='h-3/4' src={logo} alt="" />  Little  <span className='text-green-600 text-4xl'>LA</span>mb</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/alltoys'>All Toys</Link></li>
+                    <li><Link to='/mytoys'>My Toys</Link></li>
+                    <li><Link to='/addatoy'>Add A Toy</Link></li>
+                    <li><Link to='/blogs'>Blogs</Link></li>
+                    
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link to='/login' className="btn  bg-green-700">Login</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
